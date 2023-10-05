0d22fcd1ec6fa043900033d908ed9e96d3638a37
Lucas <lucasdsveras@gmail.com>
2023-10-05 04:59:56 +0100

    Add a Logo component

---
+++ src/ui/Logo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Logo() {
+  return (
+    <div>
+      <Link to="/">Logo</Link>
+    </div>
+  );
+}
+
+export default Logo;
--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,9 +1,11 @@
+import Logo from "./Logo";
 import MainNav from "./MainNav";
 
 function Sidebar() {
   return (
     <aside>
       <h1>Sidebar</h1>
+      <Logo />
       <MainNav />
     </aside>
   );
