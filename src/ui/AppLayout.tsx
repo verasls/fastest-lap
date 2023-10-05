580a295cb7ee23aadc393604647be35b993158d1
Lucas <lucasdsveras@gmail.com>
2023-10-05 22:19:52 +0100

    Style AppLayout

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -3,10 +3,12 @@ import Sidebar from "./Sidebar";
 
 function AppLayout() {
   return (
-    <div>
+    <div className="grid h-screen grid-cols-[16rem_1fr]">
       <Sidebar />
-      <main>
-        <Outlet />
+      <main className="overflow-scroll bg-neutral-50 px-12 py-10">
+        <div className="mx-auto my-0 flex max-w-7xl flex-col">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
