2d10ab1fa93f7f89e6ed07152fcb26396d9addc1
Lucas <lucasdsveras@gmail.com>
2023-10-06 02:04:24 +0100

    Style de Sidebar

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -1,26 +1,52 @@
-import { NavLink } from "react-router-dom";
+import {
+  MdOutlineDashboard,
+  MdOutlineEmojiEvents,
+  MdOutlineHandyman,
+  MdOutlineLocationOn,
+  MdOutlinePerson,
+  MdShowChart,
+} from "react-icons/md";
+import MainNavLink from "./MainNavLink";
 
 function MainNav() {
   return (
     <nav>
-      <ul>
+      <ul className="flex flex-col gap-3">
         <li>
-          <NavLink to="/app">Dashboard</NavLink>
+          <MainNavLink to="/app/dashboard">
+            <MdOutlineDashboard className="text-2xl text-neutral-400" />
+            <span>Dashboard</span>
+          </MainNavLink>
         </li>
         <li>
-          <NavLink to="/app/circuits">Circuits</NavLink>
+          <MainNavLink to="/app/drivers">
+            <MdOutlinePerson className="text-2xl text-neutral-400" />
+            <span>Drivers</span>
+          </MainNavLink>
         </li>
         <li>
-          <NavLink to="/app/drivers">Drivers</NavLink>
+          <MainNavLink to="/app/constructors">
+            <MdOutlineHandyman className="text-2xl text-neutral-400" />
+            <span>Constructors</span>
+          </MainNavLink>
         </li>
         <li>
-          <NavLink to="/app/constructors">Constructors</NavLink>
+          <MainNavLink to="/app/circuits">
+            <MdOutlineLocationOn className="text-2xl text-neutral-400" />
+            <span>Circuits</span>
+          </MainNavLink>
         </li>
         <li>
-          <NavLink to="/app/results">Results</NavLink>
+          <MainNavLink to="/app/results">
+            <MdOutlineEmojiEvents className="text-2xl text-neutral-400" />
+            <span>Results</span>
+          </MainNavLink>
         </li>
         <li>
-          <NavLink to="/app/plots">Plots</NavLink>
+          <MainNavLink to="/app/plots">
+            <MdShowChart className="text-2xl text-neutral-400" />
+            <span>Plots</span>
+          </MainNavLink>
         </li>
       </ul>
     </nav>
---
+++ src/ui/MainNavLink.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { buttonVariants } from "./button";
+
+interface MainNavLinkProps {
+  to: string;
+  children: React.JSX.Element | React.JSX.Element[];
+}
+
+function MainNavLink({ to, children }: MainNavLinkProps) {
+  const linkClass: string = `${buttonVariants({
+    variant: "ghost",
+  })} w-full gap-3 [&>svg]:hover:text-red-600 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-900 [&>svg]:aria-[current=page]:text-red-600`.replace(
+    "justify-center",
+    ""
+  );
+  return (
+    <NavLink to={to} className={linkClass}>
+      {children}
+    </NavLink>
+  );
+}
+
+export default MainNavLink;
