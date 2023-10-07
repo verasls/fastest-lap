cce2a046ac39788e4678b27ce39a9edcb42ddf53
Lucas <lucasdsveras@gmail.com>
2023-10-07 22:42:57 +0100

    Turn off no-explicit-any rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,7 @@ module.exports = {
       "react/react-in-jsx-scope": "off",
       "no-unused-vars": ["off"],
       "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/no-explicit-any": "off",
       "react/prop-types": [2, { "ignore": ["className"] }]
     }
 }
