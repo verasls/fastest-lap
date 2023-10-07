b69ec1ae5a89c1ce21b3d58dc4c5a54d57f5cf5b
Lucas <lucasdsveras@gmail.com>
2023-10-07 02:22:43 +0100

    Create Spinner component

---
+++ src/ui/Spinner.tsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="mx-auto my-12 flex h-16 w-16 animate-spin items-center  justify-center rounded-full bg-gradient-to-tr from-red-200 to-red-600">
+      <div className="h-12 w-12 rounded-full bg-neutral-50"></div>
+    </div>
+  );
+}
+
+export default Spinner;
