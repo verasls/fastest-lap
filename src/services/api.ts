ca4acd8ce10f69de7cc5157a933fe4ae0a36b71d
Lucas <lucasdsveras@gmail.com>
2023-10-08 01:21:34 +0100

    Change sessions to an array of objects

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,13 +19,13 @@ export async function getNextRace(
     raceName: race.raceName,
     circuitName: race.Circuit.circuitName,
     country: race.Circuit.Location.country,
-    sessions: {
-      session1: {
+    sessions: [
+      {
         sessionName: "Practice 1",
         sessionDate: race.FirstPractice.date,
         sessionTime: race.FirstPractice.time,
       },
-      session2: {
+      {
         sessionName: race.Sprint ? "Qualifying" : "Practice 2",
         sessionDate: race.Sprint
           ? race.Qualifying.date
@@ -34,7 +34,7 @@ export async function getNextRace(
           ? race.Qualifying.time
           : race.SecondPractice.time,
       },
-      session3: {
+      {
         sessionName: race.Sprint ? "Sprint Shootout" : "Practice 3",
         sessionDate: race.Sprint
           ? race.SecondPractice.date
@@ -43,17 +43,17 @@ export async function getNextRace(
           ? race.SecondPractice.time
           : race.ThirdPractice.time,
       },
-      session4: {
+      {
         sessionName: race.Sprint ? "Sprint" : "Qualifying",
         sessionDate: race.Sprint ? race.Sprint.date : race.Qualifying.date,
         sessionTime: race.Sprint ? race.Sprint.time : race.Qualifying.time,
       },
-      session5: {
+      {
         sessionName: "Race",
         sessionDate: race.date,
         sessionTime: race.time,
       },
-    },
+    ],
   }))
     .filter((race) => race.date >= currentDate)
     .at(0);
--- src/services/api.types.ts
+++ src/services/api.types.ts
@@ -5,12 +5,19 @@ export interface Race {
   time: string;
   circuitName: string;
   country: string;
+  sessions: Array<Session>;
   Circuit?: any;
   FirstPractice?: any;
   SecondPractice?: any;
   ThirdPractice?: any;
   Sprint?: any;
   Qualifying?: any;
+}
+
+export interface Session {
+  sessionName: string;
+  sessionDate: string;
+  sessionTime: string;
 }
 
 export interface MRData {
