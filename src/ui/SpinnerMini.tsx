6b663270edce18c91bfca9291f27cd2262b9f6ba
Lucas <lucasdsveras@gmail.com>
2023-10-10 14:34:05 +0100

    Create a smaller version of the Spinner component

---
+++ src/ui/SpinnerMini.tsx
@@ -0,0 +1,9 @@
+function SpinnerMini() {
+  return (
+    <div className="m-auto flex h-4 w-4 animate-spin items-center  justify-center rounded-full bg-gradient-to-tr from-red-200 to-red-600">
+      <div className="h-2 w-2 rounded-full bg-neutral-50"></div>
+    </div>
+  );
+}
+
+export default SpinnerMini;
