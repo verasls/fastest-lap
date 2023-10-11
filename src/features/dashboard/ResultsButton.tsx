7b8c7937c12c19f06e4d9092d52e37bcc05d0f16
Lucas <lucasdsveras@gmail.com>
2023-10-11 22:04:38 +0100

    Remove background-color change on hover

--- src/features/dashboard/ResultsButton.tsx
+++ src/features/dashboard/ResultsButton.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 function ResultsButton() {
   return (
-    <Button className="bg-red-600 hover:bg-red-500" size="sm" asChild>
+    <Button className="w-11/12 bg-red-600 hover:bg-red-600" size="sm" asChild>
       <NavLink to="/app/results">Results</NavLink>
     </Button>
   );
