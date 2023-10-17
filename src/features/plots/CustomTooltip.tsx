1ce3e295b1a8ad054a3c22526b23fab23b5f5df3
Lucas <lucasdsveras@gmail.com>
2023-10-17 22:36:40 +0100

    Do not show tooltip if round is 0

--- src/features/plots/CustomTooltip.tsx
+++ src/features/plots/CustomTooltip.tsx
@@ -8,6 +8,8 @@ export default function CustomTooltip({
   if (active && payload && payload.length) {
     const sorted = payload.sort((a, b) => b.value - a.value);
 
+    if (!sorted.at(0)?.payload.round) return null;
+
     return (
       <div className="border bg-white/90 p-2 shadow-sm">
         <p className="pb-2 font-medium text-neutral-700">Round {label}</p>
