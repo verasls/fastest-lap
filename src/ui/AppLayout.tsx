e327dd9bdf5d58795d65f8c2d6a42a585403be68
Lucas <lucasdsveras@gmail.com>
2023-10-26 01:31:11 +0100

    Rename handler function

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -6,14 +6,14 @@ import MobileHeader from "./MobileHeader";
 export default function AppLayout() {
   const [openNav, setOpenNav] = useState<boolean>(false);
 
-  function handleClick() {
+  function handleOpenMobileNav() {
     setOpenNav((open: boolean) => !open);
   }
 
   return (
     <div className="grid lg:h-screen lg:grid-cols-[14rem_1fr]">
       <Sidebar />
-      <MobileHeader open={openNav} onClick={handleClick} />
+      <MobileHeader open={openNav} onClick={handleOpenMobileNav} />
       <main className="overflow-scroll bg-neutral-50 px-2 py-10 pt-20 sm:pt-24 md:px-12 lg:pt-10">
         <div className="mx-auto my-0 flex min-h-screen max-w-7xl flex-col gap-6 sm:gap-8">
           <Outlet />
