3c96bd0c0ac415068a774edd551427c71fdfe9f6
Lucas <lucasdsveras@gmail.com>
2023-10-27 03:21:30 +0100

    Change api urls protocol to https

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL_ERGAST = "http://ergast.com/api/f1";
-export const API_URL_TIME = "http://api.timezonedb.com/v2.1";
+export const API_URL_ERGAST = "https://ergast.com/api/f1";
+export const API_URL_TIME = "https://api.timezonedb.com/v2.1";
